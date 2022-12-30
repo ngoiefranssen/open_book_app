481989e5c56a0b924ebae8fb8eddf178f48b5eb3
Franssen <ngoiefranssen04@gmail.com>
2022-12-30 14:59:28 +0200

    Wip

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () =>{
+    return(
+        <>
+        <div className='header'>
+            <div className=''>
+
+            </div>
+            <div className=''>
+                
+            </div>
+        </div>
+        </>
+    )
+}
