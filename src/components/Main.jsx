1cbcbe6bb0ed4ca391aca32947a549d680e8b667
Franssen <ngoiefranssen04@gmail.com>
2023-01-03 08:59:14 +0200

    Wip

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 
-const Main = () =>{
+const Main = () => {
     return(
         <>
-        <div className='header'>
-            <div className='row1'>
-                <h1>A room without book is like <br/> a boby without a soul</h1>
-            </div>
-            <div className='row2'>
-                <h2>Find your book</h2>
-                <div className='search'>
-                    <input type='text' placeholder='Enter your book name....' />
+            <div className='header'>
+                <div className='row1'>
+                    <h1>A room without book is like <br/> a boby without a soul</h1>
+                </div>
+                <div className='row2'>
+                    <h2>Find your book</h2>
+                    <div className='search'>
+                        <input type='text' placeholder='Enter your book name....' />
+                        <button><i className="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                    <img src="./images/imgbook_1.png" alt="" />
                 </div>
             </div>
-        </div>
         </>
     );
-};
+}; 
 
 export default Main
